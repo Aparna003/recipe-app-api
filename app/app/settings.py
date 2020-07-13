3e349d2df1797124440cccce8f7abb3640dfb6be
Aparna003 <aparnashankar004@gmail.com>
2020-07-13 15:34:26 +0530

    Updated to support custom  user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,9 +76,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+      'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    
 }
 
 
